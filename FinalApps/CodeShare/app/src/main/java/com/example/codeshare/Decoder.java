170fe49884242519b82870fe13d54061b05e76e7
stepin104958 <69075307+stepin104958@users.noreply.github.com>
2020-12-17 16:38:24 +0530

    fd

---
+++ FinalApps/CodeShare/app/src/main/java/com/example/codeshare/Decoder.java
@@ -0,0 +1,19 @@
+package com.example.codeshare;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Decoder extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.decoder_layout,container,false);
+    }
+}
